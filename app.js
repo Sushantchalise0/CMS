e4c55f0f4a4c24f24f21818b438484fa68d50bff
SUSHANT CHALISE <punkasylum3@gmail.com>
2018-08-15 11:23:58 +0545

    backend commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const upload = require('express-fileupload');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+mongoose.connect('mongodb://localhost:27017/cms', { useNewUrlParser: true }).then((db) => {
+
+    console.log('MONGO connected');
+}).catch(error => console.log(error));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//SET VIEW ENGINE
+const {select, generateTime} = require('./helpers/handlebars-helpers');
+app.engine('handlebars', exphbs({defaultLayout: 'home', helpers: {select: select, generateTime: generateTime}}));
+app.set('view engine', 'handlebars');
+
+//METHOD OVERRIDE   
+app.use(methodOverride('_method'));
+
+//UPLOAD MIDDLEWARE
+app.use(upload());
+
+// BODY PARSER
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//SESSIONS
+app.use(session({
+
+    secret: 'sushantChaliseCodingCMS',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(flash());
+
+//LOCAL VARIABLES USING MIDDLEWARE
+app.use((req, res, next) => {
+
+    res.locals.success_message = req.flash('success_message');
+    next();
+});
+
+//LOAD ROUTES
+const home = require('./routes/home/index');
+const admin = require('./routes/admin/index');
+const posts = require('./routes/admin/posts');
+
+//USE ROUTES
+app.use('/', home);
+app.use('/admin', admin);
+app.use('/admin/posts', posts);
+
+app.listen(4500, () => {
+
+    console.log(`listening on port 4500`);
+
+});
