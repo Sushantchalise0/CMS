a242a4d8ba21ef79b8dde5e54eb19f1e4d2b1169
SUSHANT CHALISE <punkasylum3@gmail.com>
2018-08-16 14:23:04 +0545

    also register

--- routes/home/index.js
+++ routes/home/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../../models/Post');
 const Category = require('../../models/Category');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 
 router.all('/*', (req, res, next) => {
@@ -34,6 +36,75 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
 
     res.render('home/register');
+});
+
+router.post('/register', (req, res) => {
+
+    let errors = [];
+    if(!req.body.firstName){
+
+        errors.push({message: 'please add a firstname'});
+    }
+
+    if(!req.body.lastName){
+
+        errors.push({message: 'please add a lastname'});
+    }
+
+    if(!req.body.email){
+
+        errors.push({message: 'please add a email'});
+    }
+
+    if(!req.body.password){
+
+        errors.push({message: 'please add a password'});
+    }
+
+    if(req.body.password !== req.body.passwordConfirm){
+
+        errors.push({message: 'password field dont match'});
+    }
+
+    if(errors.length > 0){
+
+        res.render('home/register', {
+            errors: errors,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+        })
+    }else{
+
+        User.findOne({email: req.body.email}).then(user => {
+
+            if(!user){
+
+                const newUser = new User({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    password: req.body.password
+                });
+            
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                       
+                        newUser.password = hash;
+                        newUser.save().then(savedUser => {
+            
+                            req.flash('success_message', `User ${newUser.firstName} was successfully created`);
+                            res.redirect('/login');
+                        });  
+                    });
+                });
+            } else {
+
+                req.flash('error_message', `User already exists`);
+                res.redirect('/register');
+            }
+        });
+    }   
 });
 
 router.get('/post/:id', (req, res) => {
